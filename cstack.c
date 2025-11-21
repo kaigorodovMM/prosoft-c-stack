bef2d72ba2afd4b1e4bbb3d8f14fe720ca82af83
kaigorodovMM <147080391+kaigorodovMM@users.noreply.github.com>
2025-11-21 20:38:54 +0500

    убраны ошибки с const

--- cstack.c
+++ cstack.c
@@ -83,7 +83,7 @@ void stack_free(const hstack_t hstack)
         while (entry->reserved > 0) {
             stack_t del = entry->stack;
 
-            stack_t next = del->prev;
+            stack_t next = (stack_t)del->prev;
 
             free(del);
 
@@ -127,7 +127,7 @@ void stack_push(const hstack_t hstack, const void* data_in, const unsigned int s
         if (new_st != NULL) { // поверка malloc
             (&g_table.entries)[hstack]->reserved += 1;
             new_st->size = size;
-            memcpy(data_in, new_st->data, size);
+            memcpy((void* )data_in, new_st->data, size);
             new_st->prev = (&g_table.entries)[hstack]->stack;
             (&g_table.entries)[hstack]->reserved++;
             (&g_table.entries)[hstack]->stack = new_st;
@@ -144,7 +144,7 @@ unsigned int stack_pop(const hstack_t hstack, void* data_out, const unsigned int
     if (stack_valid_handler(hstack) == 0 && data_out != NULL && size != 0) {
         memcpy((&g_table.entries)[hstack]->stack->data, data_out, size);
         stack_t del = (&g_table.entries)[hstack]->stack;
-        (&g_table.entries)[hstack]->stack = (&g_table.entries)[hstack]->stack->prev;
+        (&g_table.entries)[hstack]->stack = (stack_t)(&g_table.entries)[hstack]->stack->prev;
         (&g_table.entries)[hstack]->reserved--;
         free(del);
         return size;
